7f8e1538ce2d4891d315140280138f2c93a20ed7
Nico Saueressig <nico@nicosaueressig.de>
2014-04-26 17:10:34 +0100

    made it work

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,19 +1,14 @@
 class Array
 
-  # def my_inject(initial_value=0)
-  # 	a = each{|x| puts x}
-  # 	a{ yield } 
-  # end
-
-  def my_inject(initial_value=0)
-    if block_given?
-      acc = self.first
-      self[1..-1].each do |element|
-		@result += yield(a,element)
-		self.delete(0)
-	end
-end
-	@result
-   end
+  def my_inject(*initial_value)
+    array = initial_value + self
+    acc, *tail = array
+    tail.each do |val|
+      acc = yield(acc, val)
+    end
+    acc
+  end
 
 end
+
+#all hail the splat emperor, splat operator I mean
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -9,9 +9,8 @@ describe 'How inject works' do
     end
 
     it 'returns 4 for an array 1,2 with initial value of 1' do
-      expect([1,2].my_inject(1) { |x,y| x + y }).to eq 4
+      expect([1,2].my_inject(1){ |x,y| x + y }).to eq 4
     end
-
 
   end
 
@@ -22,7 +21,7 @@ describe 'How inject works' do
     end
 
     it 'returns -2 for an array 1,2 with initial value of 1' do
-      expect([1,2].my_inject(1) { |x,y| x - y }).to eq -2
+      expect([1,2].my_inject(1){ |x,y| x - y }).to eq -2
     end
 
   end
@@ -34,27 +33,46 @@ describe 'How inject works' do
     end
 
     it 'returns 3 for an array 1,2 with initial value of 1' do
-      expect([1,2].my_inject(1) { |x,y| x * y }).to eq 2
+      expect([1,2].my_inject(1){ |x,y| x * y }).to eq 2
     end
+
+  end
 
   context 'division' do
 
     it 'returns 0 for an array with 1,2' do
-      expect([1,2].my_inject{ |x,y| x / y }).to eq 1
+      expect([1,2].my_inject{ |x,y| x / y }).to eq 0
     end
 
     it 'returns 0 for an array 1,2 with initial value of 1' do
-      expect([1,2].my_inject(1) { |x,y| x / y }).to eq 0
+      expect([1,2].my_inject(1){ |x,y| x / y }).to eq 0
     end
 
   end
 
-context 'addition of string'
-  
-  it "returns HelloThere for array Hello there" do
-    expect(["Hello", "There"].my_inject{|x, y| x + y}).to eq "HelloThere"
+  context 'addition of string' do
+
+    it "returns HelloThere for array Hello there" do
+      expect(["Hello", "There"].my_inject{|x, y| x + y}).to eq "HelloThere"
+    end
+
   end
 
-end
+
+  context 'hardcore' do
+
+    it 'returns 121 for array of 1,2,3,4,5,6 with initial value of 100' do
+      expect([1,2,3,4,5,6].my_inject(100){|a,b| a + b}).to eq 121
+    end
+
+  end
+
+  it 'can be passed a block made by calling to_proc' do
+    expect([1,2,3].my_inject(&:+)).to eq(6)
+  end
+
+  it 'can take in an empty array' do
+    expect([1,2].my_inject(&:[])).to eq 0
+  end
 
 end
